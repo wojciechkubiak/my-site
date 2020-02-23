39372cdd7def0f15974ed589afe7838d88aabed3
Wojciech Kubiak <wgkubiak@gmail.com>
2020-02-23 23:34:14 +0100

    Landing page look

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './About.css';
+import CompImg from '../../img/comp.png';
 
 const About = () => (
   <div className="About" data-testid="About">
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './LandingPage.css';
-import {Button} from "react-bootstrap"
+import {MdKeyboardArrowDown} from "react-icons/md"
+//import {Button} from "react-bootstrap"
 
 const LandingPage = () => (
   <div className="LandingPage" data-testid="LandingPage">
+    <img className="CompImg" src={require('./../../img/comp.png')}></img>
     <div className="LandingPageName">
       <h1>Wojciech Kubiak</h1>
       <p>Student</p>
     </div>
+    <button className="BtnInfo">Więcej</button>
+    <MdKeyboardArrowDown className="ArrowDown"/>
   </div>
 );
 
