df08e7574cb8109e8874a6cb56d5d06ce49f63b6
Wojciech Kubiak <wgkubiak@gmail.com>
2020-02-24 15:31:26 +0100

    Look update

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import {MdKeyboardArrowDown} from "react-icons/md"
 
 const LandingPage = () => (
   <div className="LandingPage" data-testid="LandingPage">
-    <img className="CompImg" alt="Computer" src={require('./../../img/comp.png')} ></img>
+    {/* <img className="CompImg" alt="Computer" src={require('./../../img/comp.png')} ></img> */}
     <div className="LandingPageName">
       <h1>Wojciech Kubiak</h1>
       <p>Student</p>
