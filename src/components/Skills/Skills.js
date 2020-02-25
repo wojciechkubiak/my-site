3d6365e9e92c6709fad79c88964f11ee16f66f27
Wojciech Kubiak <wgkubiak@gmail.com>
2020-02-25 21:29:41 +0100

    Resolution for mobile smallfix

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,33 +5,33 @@ const Skills = () => (
   <div className="Skills" id="skills">
     <h1>Skills</h1>
     <div className="SkillsContainer">
-      <span className="SkillsSpan">
-        <img className="ImageStyle" onClick={() => {alert('121')}} src={require('./../../img/black_empty_js.png')} alt="React-icon" />
-        <p>Javascript</p>
+    <span className="SkillsSpan">
+        <img className="ImageStyle" src={require('./../../img/black_empty_react.png')} alt="React-icon" />
+        {/* <p>ReactJS</p> */}
       </span>
       <span className="SkillsSpan">
         <img className="ImageStyle" src={require('./../../img/black_empty_nodejs.png')} alt="NodeJS-icon" />
-        <p>NodeJS</p>
+        {/* <p>NodeJS</p> */}
       </span>
       <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_react.png')} alt="React-icon" />
-        <p>ReactJS</p>
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_html.png')} alt="HTML-icon" />
-        <p>HTML5</p>
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_css.png')} alt="CSS-icon" />
-        <p>CSS</p>
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_bootstrap.png')} alt="Bootstrap-icon" />
-        <p>Bootstrap</p>
+        <img className="ImageStyle" onClick={() => {alert('121')}} src={require('./../../img/black_empty_js.png')} alt="React-icon" />
+        {/* <p>Javascript</p> */}
       </span>
       <span className="SkillsSpan">
         <img className="ImageStyle" src={require('./../../img/black_empty_psql.png')} alt="PostgreSQL-icon" />
-        <p>PostgreSQL</p>
+        {/* <p>PostgreSQL</p> */}
+      </span>
+      <span className="SkillsSpan">
+        <img className="ImageStyle" src={require('./../../img/black_empty_html.png')} alt="HTML-icon" />
+        {/* <p>HTML5</p> */}
+      </span>
+      <span className="SkillsSpan">
+        <img className="ImageStyle" src={require('./../../img/black_empty_css.png')} alt="CSS-icon" />
+        {/* <p>CSS</p> */}
+      </span>
+      <span className="SkillsSpan">
+        <img className="ImageStyle" src={require('./../../img/black_empty_bootstrap.png')} alt="Bootstrap-icon" />
+        {/* <p>Bootstrap</p> */}
       </span>
     </div>
   </div>
