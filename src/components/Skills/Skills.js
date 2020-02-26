e61cc09b065a72fd618e23fd8d4d223db0537fc5
Wojciech Kubiak <wgkubiak@gmail.com>
2020-02-26 11:09:16 +0100

    Some animations for landing page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,31 +6,31 @@ const Skills = () => (
     <h1>Skills</h1>
     <div className="SkillsContainer">
     <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_react.png')} alt="React-icon" />
+        <img className="ImageStyle" src={require('./../../img/white_empty_react.png')} alt="React-icon" />
         {/* <p>ReactJS</p> */}
       </span>
       <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_nodejs.png')} alt="NodeJS-icon" />
+        <img className="ImageStyle" src={require('./../../img/white_empty_nodejs.png')} alt="NodeJS-icon" />
         {/* <p>NodeJS</p> */}
       </span>
       <span className="SkillsSpan">
-        <img className="ImageStyle" onClick={() => {alert('121')}} src={require('./../../img/black_empty_js.png')} alt="React-icon" />
+        <img className="ImageStyle" onClick={() => {alert('121')}} src={require('./../../img/white_empty_js.png')} alt="React-icon" />
         {/* <p>Javascript</p> */}
       </span>
       <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_psql.png')} alt="PostgreSQL-icon" />
+        <img className="ImageStyle" src={require('./../../img/white_empty_psql.png')} alt="PostgreSQL-icon" />
         {/* <p>PostgreSQL</p> */}
       </span>
       <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_html.png')} alt="HTML-icon" />
+        <img className="ImageStyle" src={require('./../../img/white_empty_html.png')} alt="HTML-icon" />
         {/* <p>HTML5</p> */}
       </span>
       <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_css.png')} alt="CSS-icon" />
+        <img className="ImageStyle" src={require('./../../img/white_empty_css.png')} alt="CSS-icon" />
         {/* <p>CSS</p> */}
       </span>
       <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/black_empty_bootstrap.png')} alt="Bootstrap-icon" />
+        <img className="ImageStyle" src={require('./../../img/white_empty_bootstrap.png')} alt="Bootstrap-icon" />
         {/* <p>Bootstrap</p> */}
       </span>
     </div>
