1288bdbce1d6557eef24c61c5f23a0a4f0feab32
Wojciech Kubiak <wgkubiak@gmail.com>
2020-02-27 02:01:09 +0100

    Some gifs

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,9 @@ import './Projects.css';
 const Projects = () => (
   <div className="Projects" data-testid="Projects" id="projects">
     <div className="ProjectsContainer">
-      <img className="ProjectsGif" src={require('./../../img/opqn.gif')} alt="React-icon" />
+      <img className="PhoneGif" src={require('./../../img/phone.gif')} onClick={(event) => alert(event.target.src)} alt="React-icon" />
+      <img className="MonitorGif" src={require('./../../img/monitor.gif')} onClick={(event) => alert(event.target.src)} alt="React-icon" />
+      <img className="TabletGif" src={require('./../../img/tablet.gif')} onClick={(event) => alert(event.target.src)} alt="React-icon" />
     </div>
   </div>
 );
