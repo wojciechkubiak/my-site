0e2018bcdcdafea3355a24ea858cf271bf3646cb
Wojciech Kubiak <wgkubiak@gmail.com>
2020-02-27 18:17:23 +0100

    Paragraph update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,13 +8,12 @@ const About = () => (
         <h1>About me</h1>
         <div className="AboutText">
           <p>
-            Hi! My name is Wojtek and I'm student of Computer Science and Measuring Systems at the University of Wroclaw.  
-            I am passionate about creating nice looking web applications using modern technologies. 
-            I got [zaraziłem się] with this passion during numerous individual and university projects. 
-            In each of them I [wykazałem się] creativy, consistency and attention to the quality of the application and code. 
-            During my studies, I also had the opportunity to participate in internships and programming internships.
-            [Podczas ich trwania] I learned how to work in the SCRUM system, how to communicate with client while receiving 
-            an application creation order and how to program with modern standards.
+          Hi! My name is Wojtek and I'm student of Computer Science and Measuring Systems at the University of Wroclaw. 
+          I am passionate about creating nice looking web applications using modern technologies. I got into it during 
+          numerous individual and university projects. In each of them I showed creativy, consistency and attention to 
+          the quality of the application and code. During my studies, I also had the opportunity to participate in 
+          internships and programming internships. During them I learned how to work in the SCRUM system, how to 
+          communicate with client while receiving an application creation order and how to program with modern standards.
           </p>
         </div>
       </div>    
