2f2fcf24e457267a63eb357757bddfc414b5424b
Wojciech Kubiak <wgkubiak@gmail.com>
2020-02-29 14:16:25 +0100

    Animations for skills and about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './About.css';
+import {InView} from "react-intersection-observer";
 
-const About = () => (
-  <div className="About" data-testid="About" id="about"> 
-      <div className="AboutContainer">     
-      <img className="AboutImg" alt="Writing" src={require('./../../img/writing.png')} ></img>     
-        <h1>About me</h1>
-        <div className="AboutText">
-          <p>
-          Hi! My name is Wojtek and I'm student of Computer Science and Measuring Systems at the University of Wroclaw. 
-          I am passionate about creating nice looking web applications using modern technologies. I got into it during 
-          numerous individual and university projects. In each of them I showed creativy, consistency and attention to 
-          the quality of the application and code. During my studies, I also had the opportunity to participate in 
-          internships and programming internships. During them I learned how to work in the SCRUM system, how to 
-          communicate with client while receiving an application creation order and how to program with modern standards.
-          </p>
-        </div>
-      </div>    
-  </div>
-);
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: [
+        "AboutContainer"
+      ]
+    };
+    this.changeClass = this.changeClass.bind(this);
+  }
+
+  changeClass = (val) => {
+    
+    if(val) {
+      this.setState({ mode: ["AboutContainer", "AboutAnim"] }, () => console.log(this.state.mode))
+    }
+  }
+
+  render() {
+    return (
+      <InView as="div" data-testid="About" id="about" onChange={(inView, entry) => this.changeClass(inView)}>
+      <div className={this.state.mode.join(' ')}>     
+        <img className="AboutImg" alt="Writing" src={require('./../../img/writing.png')} ></img>     
+          <h1>About me</h1>
+          <div className="AboutText">
+            <p>
+            Hi! My name is Wojtek and I'm student of Computer Science and Measuring Systems at the University of Wroclaw. 
+            I am passionate about creating nice looking web applications using modern technologies. I got into it during 
+            numerous individual and university projects. In each of them I showed creativy, consistency and attention to 
+            the quality of the application and code. During my studies, I also had the opportunity to participate in 
+            internships and programming internships. During them I learned how to work in the SCRUM system, how to 
+            communicate with client while receiving an application creation order and how to program with modern standards.
+            </p>
+          </div>
+        </div>    
+    </InView>
+    )
+  }
+}
+
 
 // TODO: resize is a little bit problematic 
 
 export default About;
+
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,41 +1,99 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Skills.css";
+import { InView } from "react-intersection-observer";
 
-//TODO: Info onClick on skill img
-const Skills = () => (
-  <div className="Skills" id="skills">
-    <h1>Skills</h1>
-    <div className="SkillsContainer">
-    <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/white_empty_react.png')} alt="React-icon" />
-        {/* <p>ReactJS</p> */}
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/white_empty_nodejs.png')} alt="NodeJS-icon" />
-        {/* <p>NodeJS</p> */}
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" onClick={() => {alert('121')}} src={require('./../../img/white_empty_js.png')} alt="React-icon" />
-        {/* <p>Javascript</p> */}
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/white_empty_psql.png')} alt="PostgreSQL-icon" />
-        {/* <p>PostgreSQL</p> */}
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/white_empty_html.png')} alt="HTML-icon" />
-        {/* <p>HTML5</p> */}
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/white_empty_css.png')} alt="CSS-icon" />
-        {/* <p>CSS</p> */}
-      </span>
-      <span className="SkillsSpan">
-        <img className="ImageStyle" src={require('./../../img/white_empty_bootstrap.png')} alt="Bootstrap-icon" />
-        {/* <p>Bootstrap</p> */}
-      </span>
-    </div>
-  </div>
-);
+class Skills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: ["SkillsContainer"]
+    };
+    this.changeClass = this.changeClass.bind(this);
+  }
+
+  changeClass = val => {
+    if (val) {
+      this.setState({ mode: ["SkillsContainer", "SkillsAnim"] }, () =>
+        console.log(this.state.mode)
+      );
+    }
+  };
+
+  render() {
+    return (
+      <InView
+        as="div"
+        data-testid="Skills"
+        id="skills"
+        onChange={(inView, entry) => this.changeClass(inView)}
+      >
+        <h1>Skills</h1>
+        <div className={this.state.mode.join(" ")}>
+          <span className="SkillsSpan">
+            <img
+              className="ImageStyle"
+              src={require("./../../img/white_empty_react.png")}
+              alt="React-icon"
+            />
+            {/* <p>ReactJS</p> */}
+          </span>
+          <span className="SkillsSpan">
+            <img
+              className="ImageStyle"
+              src={require("./../../img/white_empty_nodejs.png")}
+              alt="NodeJS-icon"
+            />
+            {/* <p>NodeJS</p> */}
+          </span>
+          <span className="SkillsSpan">
+            <img
+              className="ImageStyle"
+              onClick={() => {
+                alert("121");
+              }}
+              src={require("./../../img/white_empty_js.png")}
+              alt="React-icon"
+            />
+            {/* <p>Javascript</p> */}
+          </span>
+          <span className="SkillsSpan">
+            <img
+              className="ImageStyle"
+              src={require("./../../img/white_empty_psql.png")}
+              alt="PostgreSQL-icon"
+            />
+            {/* <p>PostgreSQL</p> */}
+          </span>
+          <span className="SkillsSpan">
+            <img
+              className="ImageStyle"
+              src={require("./../../img/white_empty_html.png")}
+              alt="HTML-icon"
+            />
+            {/* <p>HTML5</p> */}
+          </span>
+          <span className="SkillsSpan">
+            <img
+              className="ImageStyle"
+              src={require("./../../img/white_empty_css.png")}
+              alt="CSS-icon"
+            />
+            {/* <p>CSS</p> */}
+          </span>
+          <span className="SkillsSpan">
+            <img
+              className="ImageStyle"
+              src={require("./../../img/white_empty_bootstrap.png")}
+              alt="Bootstrap-icon"
+            />
+            {/* <p>Bootstrap</p> */}
+          </span>
+        </div>
+      </InView>
+    );
+  }
+}
+
+// TODO: resize is a little bit problematic
 
 export default Skills;
