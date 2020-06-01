667196e4c5b81956653d7bdcadaf5579b5f631eb
wojciechkubiak <wgkubiak@gmail.com>
2020-06-01 14:16:40 +0200

    MD, Resume update and styling of social button

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -3,8 +3,7 @@ import "./SocialMedia.css";
 import { FaLinkedinIn, FaFacebookF, FaGit } from "react-icons/fa";
 import { IoMdArrowRoundBack, IoMdArrowRoundForward } from "react-icons/io";
 
-const SocialMedia = ({ initIconState = false }) => {
-  
+const SocialMedia = () => {
   const facebookAdress = "https://www.facebook.com/wojciechkubiakfb";
   const linkedinAdress = "https://www.linkedin.com/in/wojciechkubiakin";
   const gitAdress = "https://www.github.com/wojciechkubiak";
@@ -12,38 +11,63 @@ const SocialMedia = ({ initIconState = false }) => {
 
   const socialHandler = () => setShowSocial(!showSocial);
 
+  const btnStyle = ["social--btn-hide", showSocial ? " " : "social--btn-show"];
+
   return (
     <>
-        <button 
-          style={{backgroundColor: "#ffffff", outline: "none", borderTopLeftRadius: "2em", border: "none", borderBottomLeftRadius: "2em", borderLeft: "2px solid #ffffff", borderTop: "2px solid #ffffff", borderBottom: "2px solid #ffffff", borderRight: "1px solid #ffffff", width: "3em", height: "3em"}}
-          onClick={socialHandler}
-        >
-          {showSocial && (
-            <IoMdArrowRoundBack style={{color: "#424142"}}/>
-          )}
-          {!showSocial && (
-             <IoMdArrowRoundForward style={{color: "#424142"}}/>
-          )}
-        </button>
-        {showSocial && (
-         <>
-        <a href={facebookAdress}>
-           <button style={{backgroundColor: "#424142", border: "none", borderTop: "2px solid #ffffff", borderBottom: "2px solid #ffffff", width: "3em", height: "3em"}}>
-             <FaFacebookF style={{color: "#ffffff"}}/>
-           </button>
-         </a>
-         <a href={linkedinAdress}>
-           <button style={{backgroundColor: "#424142", border: "none", borderTop: "2px solid #ffffff", borderBottom: "2px solid #ffffff", width: "3em", height: "3em"}}>
-             <FaLinkedinIn style={{color: "#ffffff"}}/>
-           </button>
-         </a>
-         <a href={gitAdress}>
-         <button style={{backgroundColor: "#424142", border: "none", borderTop: "2px solid #ffffff", borderBottom: "2px solid #ffffff", borderRight: "2px solid #ffffff", borderTopRightRadius: "2em", borderBottomRightRadius: "2em", width: "3em", height: "3em"}}>
-           <FaGit style={{color: "#ffffff"}}/>
-         </button>
-        </a>
-         </>
-        )}
+      <button className={btnStyle.join(" ")} style={{outline: "none"}} onClick={socialHandler}>
+        {showSocial && <IoMdArrowRoundBack style={{ color: "#424142" }} />}
+        {!showSocial && <IoMdArrowRoundForward style={{ color: "#424142" }} />}
+      </button>
+      {showSocial && (
+        <>
+          <a href={facebookAdress}>
+            <button
+              style={{
+                backgroundColor: "#424142",
+                border: "none",
+                borderTop: "2px solid #ffffff",
+                borderBottom: "2px solid #ffffff",
+                width: "3em",
+                height: "3em",
+              }}
+            >
+              <FaFacebookF style={{ color: "#ffffff" }} />
+            </button>
+          </a>
+          <a href={linkedinAdress}>
+            <button
+              style={{
+                backgroundColor: "#424142",
+                border: "none",
+                borderTop: "2px solid #ffffff",
+                borderBottom: "2px solid #ffffff",
+                width: "3em",
+                height: "3em",
+              }}
+            >
+              <FaLinkedinIn style={{ color: "#ffffff" }} />
+            </button>
+          </a>
+          <a href={gitAdress}>
+            <button
+              style={{
+                backgroundColor: "#424142",
+                border: "none",
+                borderTop: "2px solid #ffffff",
+                borderBottom: "2px solid #ffffff",
+                borderRight: "2px solid #ffffff",
+                borderTopRightRadius: "2em",
+                borderBottomRightRadius: "2em",
+                width: "3em",
+                height: "3em",
+              }}
+            >
+              <FaGit style={{ color: "#ffffff" }} />
+            </button>
+          </a>
+        </>
+      )}
     </>
   );
 };
